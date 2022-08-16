b82c1c7df6b09e1a8d5e0d3024439c4cce5871f3
LeoCodeRu <leo@leocode.ru>
2022-08-16 19:12:07 +0300

    HomeWork 05 Material Complete

---
+++ 05_MaterialCupertino/material_cupertino/lib/data_img.dart
@@ -0,0 +1,38 @@
+Map<String, dynamic> dataImg = {
+  'Photo': [
+    'https://picsum.photos/1200/501',
+    'https://picsum.photos/1200/502',
+    'https://picsum.photos/1200/503',
+    'https://picsum.photos/1200/504',
+    'https://picsum.photos/1200/505',
+    'https://picsum.photos/1200/506',
+    'https://picsum.photos/1200/507',
+    'https://picsum.photos/1200/508',
+    'https://picsum.photos/1200/509',
+    'https://picsum.photos/1200/510',
+  ],
+  'Chat': [
+    'https://picsum.photos/1200/511',
+    'https://picsum.photos/1200/512',
+    'https://picsum.photos/1200/513',
+    'https://picsum.photos/1200/514',
+    'https://picsum.photos/1200/515',
+    'https://picsum.photos/1200/516',
+    'https://picsum.photos/1200/517',
+    'https://picsum.photos/1200/518',
+    'https://picsum.photos/1200/519',
+    'https://picsum.photos/1200/520',
+  ],
+  'Albums': [
+    'https://picsum.photos/1200/521',
+    'https://picsum.photos/1200/522',
+    'https://picsum.photos/1200/523',
+    'https://picsum.photos/1200/524',
+    'https://picsum.photos/1200/525',
+    'https://picsum.photos/1200/526',
+    'https://picsum.photos/1200/527',
+    'https://picsum.photos/1200/528',
+    'https://picsum.photos/1200/529',
+    'https://picsum.photos/1200/530',
+  ]
+};
