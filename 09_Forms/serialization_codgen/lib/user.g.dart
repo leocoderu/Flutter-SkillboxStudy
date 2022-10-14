5d913fe95477cca84e3e7e0d5555fda0824a078e
LeoCodeRu <leo@leocode.ru>
2022-10-14 08:31:36 +0300

    refactor folder FormsHotel to Forms/hotel

---
+++ 09_Forms/serialization_codgen/lib/user.g.dart
@@ -0,0 +1,38 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) => User(
+      id: json['id'] as int,
+      name: json['name'] as String? ?? 'Guest',
+      username: json['user_name'] as String,
+      email: json['email'] as String,
+      address: UserAddress.fromJson(json['address'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'user_name': instance.username,
+      'email': instance.email,
+      'address': instance.address.toJson(),
+    };
+
+UserAddress _$UserAddressFromJson(Map<String, dynamic> json) => UserAddress(
+      street: json['street'] as String,
+      suite: json['suite'] as String,
+      city: json['city'] as String,
+      zipcode: json['zipcode'] as String,
+    );
+
+Map<String, dynamic> _$UserAddressToJson(UserAddress instance) =>
+    <String, dynamic>{
+      'street': instance.street,
+      'suite': instance.suite,
+      'city': instance.city,
+      'zipcode': instance.zipcode,
+    };
