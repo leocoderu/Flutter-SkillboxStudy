4ee8763a861d6118505f3de79d64913392c4d82d
LeoCodeRu <leo@leocode.ru>
2022-11-11 11:15:22 +0300

    Add Simple State Management

---
+++ 13_StateManagement/simple_sm/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class Counter with ChangeNotifier {
+  int value = 0;
+  final int v;
+
+  Counter({required this.v});
+
+  void increment() {
+    value += v;
+    notifyListeners();
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<Counter>(
+      create: (_) => Counter(v: 2),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    //print('buid');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Consumer<Counter>(
+                builder: (context, state, child) => Text(
+                    state.value.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: context.read<Counter>().increment,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
